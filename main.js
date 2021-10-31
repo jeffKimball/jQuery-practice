c3d4af482c0b3b48d34c906066adc1236bf3c905
Jeff Kimball <jeffkimballis@gmail.com>
2021-10-30 22:26:44 -0500

    practice fade fadeto slideUp slideDown hide show toggle

---
+++ main.js
@@ -0,0 +1,19 @@
+$(function(){
+    // $('.red-box').fadeTo(1000, 0)    
+    // $(".red-box").hide(1000)
+    // $(".red-box").show(1000)
+    // $(".red-box").fadeTo(1000, 0.2)
+    // $('.green-box').fadeTo(1500, .5)
+    // $('.green-box').toggle()
+    // $('.green-box').toggle()
+    // $('.blue-box').slideUp(500)
+    // $('.blue-box').slideDown(500)  
+
+    $('p').hide()
+    // $('p').slideToggle(800)
+    // $('p').slideToggle(800)
+    $('p').slideDown(800)
+
+
+    
+})  
