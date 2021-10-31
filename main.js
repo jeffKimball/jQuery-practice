9f362b0ea7a54a037582f6f9188a5a1af7aea2b8
Jeff Kimball <jeffkimballis@gmail.com>
2021-10-30 23:22:49 -0500

    practice animate animation chaining delay callback functions

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 $(function(){
-    // $('.red-box').fadeTo(1000, 0)    
+    // $('.blue-box').animate({
+    //     "margin-left": "+=150px",
+    //     "opacity" : "0"
+    // }, 600,"linear")
+
+    // $('.blue-box').animate({
+    //     marginLeft: "+=100px",
+    //     opacity : "0",
+    //     height : "50px",
+    //     width : "50px",
+    //     marginTop: "25px"
+    // }, 600,"linear")
+
+    // $('.blue-box').animate({
+    //     "margin-left": "-=200px"
+    // }, 1000,"linear")
+
+    // $('.blue-box').animate({
+    //     marginLeft: "+=150px",
+    //     opacity : "0"
+    // }, 600,"linear")
+
+    // $("p").animate({
+    //     fontSize: "20px"
+    // }, 1000)
+
+    // $(".red-box").fadeTo(1000,0.2)
+    // $(".green-box").delay(1000).fadeTo(1000,0.4)
+    // $(".blue-box").delay(2000).fadeTo(1000,0.6)
+
+    // $(".red-box").fadeTo(1000, 0.2, function(){
+    //     $(".green-box").fadeTo(1000, 0.2, function(){
+    //         $(".blue-box").fadeTo(1000, 0.2)
+    //     })
+    // })
+
+
+    
+})  
+
+
+
+
+
+
+// $('.red-box').fadeTo(1000, 0)    
     // $(".red-box").hide(1000)
     // $(".red-box").show(1000)
     // $(".red-box").fadeTo(1000, 0.2)
@@ -9,11 +54,7 @@ $(function(){
     // $('.blue-box').slideUp(500)
     // $('.blue-box').slideDown(500)  
 
-    $('p').hide()
+    // $('p').hide()
     // $('p').slideToggle(800)
     // $('p').slideToggle(800)
-    $('p').slideDown(800)
-
-
-    
-})  
+    // $('p').slideDown(800)
